cd5af2668e14d10f6b0d7b0809716713fbe18d79
Matthias Meissner <geige.matze@gmail.com>
2023-12-05 17:49:10 +0100

    implement GAIA

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,33 @@ use std::{
     ops::{Add, Deref, Mul, Sub},
 };
 
-trait JoinSemiLattice {
+trait JoinSemiLattice: Sized {
     fn bot() -> Self;
     fn join(&self, other: &Self) -> Self;
     fn leq(&self, other: &Self) -> bool;
+
+    fn join_opt(lhs: Option<&Self>, rhs: Option<&Self>) -> Self {
+        match (lhs, rhs) {
+            (None, None) => Self::bot(),
+            (Some(x), None) => x.join(&Self::bot()),
+            (None, Some(x)) => x.join(&Self::bot()),
+            (Some(x), Some(y)) => x.join(y),
+        }
+    }
+}
+
+trait BorrowedLubIterator<T> {
+    fn borrowed_lub(self) -> T;
+}
+
+impl<T: JoinSemiLattice, I> BorrowedLubIterator<T> for I
+where
+    I: Iterator,
+    I::Item: Deref<Target = T>,
+{
+    fn borrowed_lub(self) -> T {
+        self.fold(T::bot(), |a, b| a.join(&b))
+    }
 }
 
 trait LubIterator<T> {
@@ -17,8 +40,7 @@ trait LubIterator<T> {
 
 impl<T: JoinSemiLattice, I> LubIterator<T> for I
 where
-    I: Iterator,
-    I::Item: Deref<Target = T>,
+    I: Iterator<Item = T>,
 {
     fn lub(self) -> T {
         self.fold(T::bot(), |a, b| a.join(&b))
@@ -70,7 +92,7 @@ impl<K: JoinSemiLattice + Hash + Eq, V: JoinSemiLattice + Eq> PartialTable<K, V>
             .0
             .iter()
             .filter_map(|(a, b)| if a.leq(&k) { Some(b) } else { None })
-            .lub();
+            .borrowed_lub();
 
         self.0.insert(k, value);
     }
@@ -290,15 +312,182 @@ where
     }
 }
 
+struct AtomLike {
+    predicate: FunctionSymbol,
+    variables: Vec<u32>,
+}
+
+struct Clause {
+    body_atoms: Vec<AtomLike>,
+    variable_equalities: Vec<(u32, u32)>,
+    func_equalities: Vec<(u32, AtomLike)>,
+    num_variables: u32,
+}
+
+impl Clause {
+    fn execute<AD: AbstractDomain + Clone>(
+        &self,
+        mut f: impl FnMut(Query<AD>) -> Substitution<AD>,
+        input: &Substitution<AD>,
+    ) -> Substitution<AD> {
+        let mut subst = input.extended(self.num_variables);
+
+        for atom in &self.body_atoms {
+            let local_subst = subst.rename_project(&atom.variables);
+            let query = Query {
+                predicate: atom.predicate.clone(),
+                subst: local_subst,
+            };
+            subst.extend_renamed(&atom.variables, f(query));
+        }
+
+        for &(lhs, rhs) in &self.variable_equalities {
+            subst.apply_eq_constraint(lhs, rhs);
+        }
+
+        for (lhs, rhs) in &self.func_equalities {
+            subst.apply_func_constraint(*lhs, rhs);
+        }
+
+        subst.restrict(input.len());
+        subst
+    }
+}
+
+struct Gaia {
+    program: HashMap<(FunctionSymbol, u32), Vec<Clause>>,
+}
+
+#[derive(PartialEq, Eq, Debug, Hash, PartialOrd, Ord, Clone)]
+struct FunctionSymbol(String);
+
+#[derive(Debug)]
+struct Substitution<AD>(Vec<AD>);
+
+trait AbstractDomain: JoinSemiLattice {
+    fn unify_with(&mut self, other: Self);
+    fn unify_nested(&mut self, functor: &FunctionSymbol, subterms: Vec<Option<&Self>>);
+
+    fn unify_replace_both(&mut self, other: &mut Self) {
+        let right = std::mem::replace(other, Self::bot());
+        self.unify_with(right);
+        *other = self.join(&Self::bot());
+    }
+}
+
+impl<AD> Substitution<AD> {
+    fn len(&self) -> u32 {
+        self.0.len() as u32
+    }
+}
+
+impl<AD: JoinSemiLattice> JoinSemiLattice for Substitution<AD> {
+    fn bot() -> Self {
+        Substitution(Vec::new())
+    }
+
+    fn join(&self, other: &Self) -> Self {
+        Substitution(
+            (0..std::cmp::max(self.0.len(), other.0.len()))
+                .map(|i| AD::join_opt(self.0.get(i), other.0.get(i)))
+                .collect(),
+        )
+    }
+
+    fn leq(&self, other: &Self) -> bool {
+        if self.0.len() > other.0.len() {
+            return false;
+        }
+
+        self.0.iter().zip(&other.0).all(|(a, b)| a.leq(b))
+    }
+}
+
+impl<AD: AbstractDomain + Clone> Substitution<AD> {
+    fn extended(&self, num_vars: u32) -> Self {
+        let mut res = Vec::with_capacity(num_vars as usize);
+        res.extend_from_slice(&self.0);
+        res.resize_with(num_vars as usize, AD::bot);
+        Substitution(res)
+    }
+
+    fn restrict(&mut self, num_vars: u32) {
+        self.0.resize_with(num_vars as usize, || {
+            panic!("restrict: num_vars must be less then length")
+        });
+    }
+
+    fn rename_project(&self, vars: &[u32]) -> Self {
+        Substitution(vars.iter().map(|&v| self.0[v as usize].clone()).collect())
+    }
+
+    fn extend_renamed(&mut self, vars: &[u32], updated: Self) {
+        for (var, update) in vars.iter().zip(updated.0) {
+            self.0[*var as usize].unify_with(update);
+        }
+    }
+
+    fn apply_eq_constraint(&mut self, lhs: u32, rhs: u32) {
+        if lhs == rhs {
+            return;
+        }
+
+        let higher = std::cmp::max(lhs, rhs) as usize;
+        let lower = std::cmp::min(lhs, rhs) as usize;
+
+        let (left, right) = self.0.split_at_mut(higher);
+
+        left[lower].unify_replace_both(&mut right[0]);
+    }
+
+    fn apply_func_constraint(&mut self, lhs: u32, rhs: &AtomLike) {
+        let (left, right) = self.0.split_at_mut(lhs as usize);
+        let (current, right) = right.split_first_mut().unwrap();
+
+        let subterms: Vec<_> = rhs
+            .variables
+            .iter()
+            .map(|v| match *v {
+                x if (x > lhs) => Some(&right[(x - lhs - 1) as usize]),
+                x if (x == lhs) => None,
+                _ => Some(&left[*v as usize]),
+            })
+            .collect();
+
+        current.unify_nested(&rhs.predicate, subterms);
+    }
+}
+
+struct Query<AD> {
+    subst: Substitution<AD>,
+    predicate: FunctionSymbol,
+}
+
+impl<AD: AbstractDomain + Clone> MonotoneTransform<Query<AD>> for Gaia {
+    type Output = Substitution<AD>;
+
+    fn call<F>(&self, mut f: F, query: Query<AD>) -> Self::Output
+    where
+        F: FnMut(Query<AD>) -> Self::Output,
+    {
+        self.program
+            .get(&(query.predicate, query.subst.len()))
+            .into_iter()
+            .flatten()
+            .map(|c| c.execute(&mut f, &query.subst))
+            .lub()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::{compute_fixpoint, Factorial};
 
     #[test]
     fn test_lub() {
-        use crate::LubIterator;
+        use crate::BorrowedLubIterator;
         let nums = [1 as u64, 4, 2, 42, 1341, 3];
-        assert_eq!(nums.iter().lub(), 1341);
+        assert_eq!(nums.iter().borrowed_lub(), 1341);
     }
 
     #[test]
