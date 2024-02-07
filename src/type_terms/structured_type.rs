6e01498c6afafa6c613fcd700b0646dea0d770a6
Matthias Meissner <geige.matze@gmail.com>
2024-02-07 15:12:06 +0100

    structured type skelleton

---
+++ src/type_terms/structured_type.rs
@@ -0,0 +1,81 @@
+use crate::{
+    traits::{
+        lattice::{LocalMinimum, Meet, PreOrder, Top},
+        structural::{Cons, InterpretBuiltin, TypeDomain, Uncons},
+    },
+    type_inference::Program,
+    util::tup::Tup,
+};
+
+use super::const_model::{NemoBuiltin, NemoCtor, NemoFunctor, NemoModel};
+
+#[derive(Debug, Clone)]
+pub struct StructuredType;
+
+pub type StructuredTypeConfig = ();
+
+impl PreOrder for StructuredType {
+    fn leq(&self, other: &Self) -> bool {
+        todo!()
+    }
+}
+
+impl Meet for StructuredType {
+    fn meet_with(&mut self, other: &Self) {
+        todo!()
+    }
+}
+
+impl Top for StructuredType {
+    fn is_top(&self) -> bool {
+        todo!()
+    }
+
+    fn top() -> Self {
+        todo!()
+    }
+}
+
+impl InterpretBuiltin<NemoBuiltin> for StructuredType {
+    fn interpret(builtin: NemoBuiltin, tup: Tup<Self>) -> Option<Tup<Self>> {
+        todo!()
+    }
+}
+
+impl Uncons<NemoFunctor> for StructuredType {
+    fn uncons(&self, func: &NemoFunctor) -> Option<Vec<Self>> {
+        todo!()
+    }
+}
+
+impl Cons<NemoFunctor> for StructuredType {
+    type Config = StructuredTypeConfig;
+
+    fn cons(config: &Self::Config, ctor: NemoFunctor, subterms: Vec<Self>) -> Option<Self> {
+        todo!()
+    }
+}
+
+impl Cons<NemoCtor> for StructuredType {
+    type Config = StructuredTypeConfig;
+
+    fn cons(config: &Self::Config, ctor: NemoCtor, subterms: Vec<Self>) -> Option<Self> {
+        todo!()
+    }
+}
+
+impl LocalMinimum<StructuredTypeConfig> for StructuredType {
+    fn local_minimum(key: &StructuredTypeConfig) -> Self {
+        todo!()
+    }
+}
+
+impl TypeDomain for StructuredType {
+    type Model = NemoModel;
+
+    type Config = StructuredTypeConfig;
+
+    fn configure<P>(program: &Program<P, NemoModel>) -> StructuredTypeConfig {
+        todo!()
+    }
+}
