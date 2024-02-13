418a2f58edc62285a9620acfbce0c4d6e3174baa
Matthias Meissner <geige.matze@gmail.com>
2024-02-13 17:13:15 +0100

    add example for list type inference

---
+++ src/bin/struct_inference.rs
@@ -0,0 +1,52 @@
+use fixpoints::{
+    type_inference::{
+        fixpoint::compute_fixpoint,
+        model::{BodyAtom, BodyTerm, HeadTerm, PatClause},
+        Program,
+    },
+    type_terms::{
+        const_model::{IdentConstant, NemoCtor, NemoFunctor, NestedFunctor},
+        structured_type::StructuredType,
+    },
+};
+
+fn main() {
+    let mut program = Program::new();
+    let nil = NemoFunctor::Const(IdentConstant::IriConst("<nil>".into()));
+    let cons = NemoFunctor::Nested(NestedFunctor::List {
+        tag: Some("::".into()),
+        length: 2,
+    });
+
+    program.add_rule(
+        "list",
+        PatClause {
+            head: vec![HeadTerm::Ctor(NemoCtor::Functor(nil), vec![])],
+            body_atoms: vec![],
+            body_builtins: vec![],
+            body_variables: 0,
+        },
+    );
+
+    program.add_rule(
+        "list",
+        PatClause {
+            head: vec![HeadTerm::Ctor(
+                NemoCtor::Functor(cons),
+                vec![
+                    HeadTerm::Ctor(NemoCtor::Functor(NemoFunctor::Double), vec![]),
+                    HeadTerm::Var(0),
+                ],
+            )],
+            body_atoms: vec![BodyAtom {
+                predicate: "list",
+                terms: vec![BodyTerm::Var(0)],
+            }],
+            body_builtins: vec![],
+            body_variables: 1,
+        },
+    );
+
+    let pt = compute_fixpoint("list", program.analyse::<StructuredType>());
+    println!("{pt:#?}");
+}
