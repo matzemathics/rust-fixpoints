2017dd6e505f6fe321f7d155ef129192cf79e25a
Matthias Meissner <geige.matze@gmail.com>
2024-02-15 08:13:08 +0100

    add list_list test

--- src/bin/list_list.rs
+++ src/bin/list_list.rs
@@ -18,23 +18,22 @@ fn main() {
         length: 2,
     });
 
+    let nil_clause = || PatClause {
+        head: vec![HeadTerm::Ctor(NemoCtor::Functor(nil.clone()), vec![])],
+        body_atoms: vec![],
+        body_builtins: vec![],
+        body_variables: 0,
+    };
+
     // list(nil).
-    program.add_rule(
-        "list",
-        PatClause {
-            head: vec![HeadTerm::Ctor(NemoCtor::Functor(nil), vec![])],
-            body_atoms: vec![],
-            body_builtins: vec![],
-            body_variables: 0,
-        },
-    );
+    program.add_rule("list", nil_clause());
 
     // list(__double__, ?xs) :- list(?xs).
     program.add_rule(
         "list",
         PatClause {
             head: vec![HeadTerm::Ctor(
-                NemoCtor::Functor(cons),
+                NemoCtor::Functor(cons.clone()),
                 vec![
                     HeadTerm::Ctor(NemoCtor::Functor(NemoFunctor::Double), vec![]),
                     HeadTerm::Var(0),
@@ -49,6 +48,32 @@ fn main() {
         },
     );
 
-    let pt = compute_fixpoint("list", program.analyse::<StructuredType>());
+    // list_list(nil).
+    program.add_rule("list_list", nil_clause());
+
+    // list_list(?x, ?xs) :- list(?x), list_list(?xs).
+    program.add_rule(
+        "list_list",
+        PatClause {
+            head: vec![HeadTerm::Ctor(
+                NemoCtor::Functor(cons),
+                vec![HeadTerm::Var(0), HeadTerm::Var(1)],
+            )],
+            body_atoms: vec![
+                BodyAtom {
+                    predicate: "list",
+                    terms: vec![BodyTerm::Var(0)],
+                },
+                BodyAtom {
+                    predicate: "list_list",
+                    terms: vec![BodyTerm::Var(1)],
+                },
+            ],
+            body_builtins: vec![],
+            body_variables: 2,
+        },
+    );
+
+    let pt = compute_fixpoint("list_list", program.analyse::<StructuredType>());
     println!("{pt:#?}");
 }
